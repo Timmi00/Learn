a95ac800341171b2cb5ca47e0a8bf1ac504d2424
Timmi00 <timmi00@tut.by>
2023-07-13 10:10:58 +0300

    make import from module

---
+++ 8/print_models.py
@@ -0,0 +1,9 @@
+from printing_functions import *
+
+
+unprinted = ['phone case', 'robot pendant', 'dodecahedron']
+completed = []
+print_models(unprinted[:], completed)
+show_completed_models(completed)
+print(unprinted)
+print(completed)
--- 8/printing_functions.py
+++ 8/printing_functions.py
@@ -4,17 +4,10 @@ def print_models(unprinted_designs: list,
     while unprinted_designs:
         current_design: str = unprinted_designs.pop()
         print(f'Printing model: {current_design.upper()}')
-        completed.append(current_design)
+        complete_models.append(current_design)
 
 
 def show_completed_models(completed_models: list):
     print('\nThe followed models have been printed: ')
     for completed_model in completed_models:
         print(completed_model.upper())
-
-
-unprinted = ['phone case', 'robot pendant', 'dodecahedron']
-completed = []
-print_models(unprinted[:], completed)
-show_completed_models(completed)
-print(unprinted)
