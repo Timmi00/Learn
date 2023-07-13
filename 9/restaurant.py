3000cdd2f16766b4506801c87acaae831f3d85a9
Timmi00 <timmi00@tut.by>
2023-07-13 14:05:58 +0300

    Add new attributes and methods.

--- 9/restaurant.py
+++ 9/restaurant.py
@@ -1,10 +1,20 @@
 class Restaurant:
     """простая модель ресторана"""
 
-    def __init__(self,restaurant_name: str, cuisine_type: str):
+    def __init__(self, restaurant_name: str, cuisine_type: str):
         """Иннициализация атрибутов"""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def set_number_served(self, number: int):
+        self.number_served = number
+
+    def read_number_served(self):
+        print(f'Колличество обслуженных посетителей: {self.number_served}')
+
+    def increment_number_served(self, count: int):
+        self.number_served += count
 
     def describe_restaurant(self):
         """Создание метода"""
@@ -12,17 +22,4 @@ class Restaurant:
 
     def open_restaurant(self):
         print(f'Ресторан {self.restaurant_name} открыт для посетителей')
-
-
-restaurant_blr = Restaurant('Вежа', 'Белорусской кухни')
-print(restaurant_blr.restaurant_name)
-print(restaurant_blr.cuisine_type)
-restaurant_blr.describe_restaurant()
-restaurant_blr.open_restaurant()
-restaurant_mex = Restaurant('MeXico', 'Мексиканской кухни')
-restaurant_pl = Restaurant('Pane', 'Польской кухни')
-restaurant_pl.open_restaurant()
-restaurant_pl.describe_restaurant()
-restaurant_mex.open_restaurant()
-restaurant_mex.describe_restaurant()
 
