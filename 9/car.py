ebf5f3a121da6aeddf4da33bcc58d3cc4f3d9ca7
Timmi00 <timmi00@tut.by>
2023-09-26 20:03:43 +0300

    New class

--- 9/car.py
+++ 9/car.py
@@ -29,30 +29,10 @@ class Car:
         self.odometer_reading += miles
 
 
-class Battery:
-    def __init__(self, size: int = 75):
-        self.size = size
-
-    def describe_battery(self):
-        print(f'This car has a {self.size}-kWh battery')
-
-    def get_range(self):
-        rang = 0
-        if self.size == 75:
-            rang = 260
-        elif self.size == 100:
-            rang = 315
-        print(f'This car can go about {rang} kM on a full charge')
-
-
-class ElectricCar(Car):
-
-    def __init__(self, make: str, model: str, year: int):
-        super().__init__(make, model, year)
-        self.battery = Battery()
-
-
-my_tesla = ElectricCar('tesla', 'model s', 2019)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+# my_tesla = ElectricCar('tesla', 'model s', 2019)
+# print(my_tesla.get_descriptive_name())
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
+# my_tesla.battery.upgrade_battery()
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
--- 9/restaurant.py
+++ 9/restaurant.py
@@ -22,21 +22,3 @@ class Restaurant:
 
     def open_restaurant(self):
         print(f'Ресторан {self.restaurant_name} открыт для посетителей')
-
-
-class IceCreamStand(Restaurant):
-
-    def __init__(self, restaurant_name: str, cuisine_type: str, flavors: list):
-        super().__init__(restaurant_name, cuisine_type)
-        self.flavors = flavors
-
-    def describe_flavors(self):
-        print('The restaurant have next flavors:')
-        for flavor in self.flavors:
-            print(flavor)
-
-
-mor = IceCreamStand('kafe_morozhenoe', 'all kinds of icecream', ['peach', 'mellon', 'chocolate'])
-mor.describe_flavors()
-mor.describe_restaurant()
-mor.open_restaurant()
